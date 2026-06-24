3e5440db21b9c65c0162025166e383958f2a984c
Muhammad Bahrul Widad <muhammadwidad1204@students.unnes.ac.id>
2026-06-24 10:11:19 +0700

    feat: wrap app with AuthProvider for Supabase session context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import RootLayout from './app/layout'
+import { AuthProvider } from './hooks/useAuth'
 // @ts-ignore
 import './app/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <RootLayout />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <RootLayout />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 )
